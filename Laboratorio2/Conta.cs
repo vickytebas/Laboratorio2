165e76a8e64cb548437834a12dcd13b340d4831e
victao_soluçao <vickytebas@gmail.com>
2021-03-23 21:11:56 -0300

    Add project files.

---
+++ Laboratorio2/Conta.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio2
+{
+    class Conta
+    {
+        //Atributos
+        private bool isPoupanca;
+        private int numIdentificacao;
+        private string nome;
+        private float saldoAtual;
+
+        //Construtor
+        public Conta(bool isPoupanca, int numIdentificacao, string nome, float saldoAtual)
+        {
+            this.isPoupanca = isPoupanca;
+            this.numIdentificacao = numIdentificacao;
+            this.nome = nome;
+            this.saldoAtual = saldoAtual;
+        }
+
+        //Encapsulamento
+        public bool IsPoupanca { get => isPoupanca; }
+        public int NumIdentificacao { get => numIdentificacao; }
+        public string Nome { get => nome; }
+        public float SaldoAtual { get => saldoAtual; set => saldoAtual = value; }
+
+        //Metodos
+        public void SacarDinheiro(float saque)
+        {
+            float tarifaSaque = 0;
+
+            if (SaldoAtual > 0)
+            {
+                if (IsPoupanca == true)
+                {
+                    tarifaSaque = saque * 0.2f;
+                }
+                else
+                {
+                    tarifaSaque = saque * 0.37f;
+                }
+
+                if (saque + tarifaSaque > SaldoAtual)
+                {
+                    Console.Write("{0}, saldo insuficiente para o saque. Operacao cancelada.", Nome);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    SaldoAtual -= saque + tarifaSaque;
+                }
+            }
+        }
+
+        public void DepositarDinheiro(float deposito)
+        {
+            SaldoAtual += deposito;
+        }
+
+        public void TransferirDinheiro(float transferencia, Conta receptor)
+        {
+            float tarifaTransferencia = 0;
+
+            if (IsPoupanca == true)
+            {
+                tarifaTransferencia = transferencia * 0.15f;
+            }
+            else
+            {
+                tarifaTransferencia = transferencia * 0.1f;
+            }
+
+            if (transferencia + tarifaTransferencia > saldoAtual)
+            {
+                Console.Write("{0}, saldo insuficiente para a transferencia. Operacao cancelada.", Nome);
+                Console.ReadLine();
+            }
+            else
+            {
+                SaldoAtual -= transferencia + tarifaTransferencia;
+                receptor.SaldoAtual += transferencia;
+            }
+        }
+
+        public void ChecarDinheiro()
+        {
+            Console.Write("{0}", SaldoAtual);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Laboratorio2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta contaBob = new Conta(false, 123456, "Bob Nelson", 0f);
+
+            Conta contaTestolfo = new Conta(true, 717171, "Testolfo Rocha", 0f);
+
+            Conta contaLisa = new Conta(false, 654321, "Lisa Leite", 0f);
+
+            contaBob.DepositarDinheiro(5000f);
+
+            contaLisa.DepositarDinheiro(2000f);
+
+            contaTestolfo.DepositarDinheiro(1500f);
+
+            contaBob.TransferirDinheiro(600f, contaTestolfo);
+
+            contaLisa.SacarDinheiro(250);
+
+            contaLisa.TransferirDinheiro(400, contaTestolfo);
+
+            contaTestolfo.TransferirDinheiro(1000, contaBob);
+
+            contaBob.SacarDinheiro(900);
+
+            contaBob.TransferirDinheiro(1500, contaLisa);
+
+            contaTestolfo.TransferirDinheiro(1200, contaLisa);
+
+            contaBob.SacarDinheiro(2000);
+
+            contaLisa.DepositarDinheiro(100);
+
+            contaTestolfo.TransferirDinheiro(700, contaBob);
+
+            Console.WriteLine("Total conta Bob: {0}, conta Testolfo: {1}, conta Lisa: {2}", contaBob.SaldoAtual, contaTestolfo.SaldoAtual, contaLisa.SaldoAtual);
+            Console.ReadLine();
+        }
+    }
+}
